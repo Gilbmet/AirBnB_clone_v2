889dce484b373ebc1d7a28f51db72fcae35f8d9c
Gilbmet <gilbmet254@gmail.com>
2023-04-24 04:22:29 +0300

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -28,7 +28,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     """Displays 'C' followed by the value of <text>.
-    
+
     Replaces any underscores in <text> with slashes.
     """
     text = text.replace("_", " ")
@@ -39,7 +39,7 @@ def c(text):
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
     """Displays 'Python' followed by the value of <text>.
-    
+
     Replaces any underscores in <text> with slashes.
     """
     text = text.replace("_", " ")
@@ -47,4 +47,4 @@ def python(text="is cool"):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
