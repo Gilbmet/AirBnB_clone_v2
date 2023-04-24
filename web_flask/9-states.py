d34c52a3c7f0aa5f560117c47acab1d415293667
Gilbmet <gilbmet254@gmail.com>
2023-04-24 04:37:32 +0300

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,7 +4,7 @@
 The application listens on 0.0.0.0, port 5000.
 Routes:
     /states: HTML page with a list of all State objects.
-    /states/<id>: HTML page displaying the given state with <id>.
+    /states/<int:id>: HTML page displaying the given state with <id>.
 """
 from models import storage
 from flask import Flask
@@ -20,16 +20,14 @@ def states():
     States are sorted by name.
     """
     states = storage.all("State")
-    return render_template("9-states.html", state=states)
+    return render_template("9-states.html", states=states)
 
 
-@app.route("/states/<id>", strict_slashes=False)
+@app.route("/states/<int:id>", strict_slashes=False)
 def states_id(id):
     """Displays an HTML page with info about <id>, if it exists."""
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
+    state = storage.get("State", id)
+    return render_template("9-states.html", state=state)
 
 
 @app.teardown_appcontext
